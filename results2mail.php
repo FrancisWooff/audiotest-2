88f06e8b0b57cfa4a3211d409bb92110431dd58c
FrancisW <65406277+franciswooff@users.noreply.github.com>
2020-09-22 08:28:49 +0100

    Update results2mail.php

--- results2mail.php
+++ results2mail.php
@@ -14,6 +14,9 @@ $name=$_POST[("name")];
 $lvla=$_POST[("ares")];
 $lvlb=$_POST[("bres")];
 $lvlc=$_POST[("cres")];
+
+//For each additional channel on your html test page you will need an additional line above with the appropriate letter ("dres" etc). You will then need to edit line 20 (beginning "echo") to add "<p>Slider D ".$lvld."</p>" etc. & line 22 (beginning "mail") to add "." Slider D".$lvld" etc.
+
 $mail="you@edu.salford.ac.uk";
 
 echo "<p>Thanks ".$name."</p><p>Your results have been sent to name</p><p>Slider A ".$lvla."</p><p>Slider B ".$lvlb."</p><p>Slider C ".$lvlc."</p>";
