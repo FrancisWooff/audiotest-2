e3e617bcbe59c51f583342ffecc6da68117645d0
FrancisW <65406277+franciswooff@users.noreply.github.com>
2020-09-22 16:34:34 +0100

    Update audtest.js

--- audtest.js
+++ audtest.js
@@ -1,17 +1,14 @@
-window.onVidyardAPI = (vidyardEmbed) => {
-vidyardEmbed.api.addReadyListener((_, player) => {
-
 var but = document.querySelectorAll("button");
-var vid = VidyardV4.players[0];
+var vid = document.querySelector("video");
 
 but.forEach(prsd);
 function prsd(item, index) {
     item.addEventListener("click",function(){
     var i = index;
-    vid.setAudioTrack(i);
+    vid.audioTracks[i].enabled = true;
+    vid.audioTracks[!i].enabled = false;
     })
 }
-})};
 
 var slid = document.querySelectorAll("[type=range]");
 var ipnm = document.querySelectorAll("[type=number]");
